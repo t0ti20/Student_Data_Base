224c0c153666ef664f3444f92e35121f2f56ce5f
t0ti20 <t0ti20@icloud.com>
2023-03-19 21:03:52 +0200

    Adding ID Check

--- Main.h
+++ Main.h
@@ -6,25 +6,29 @@
 /*****************************************
 ----------    GLOBAL DATA     ------------
 *****************************************/
-/*------------- Variables --------------*/
-u8 Course_ID;
-/*-------------- Define ----------------*/
-#define True             1
-#define False            0
 /*------------- Type Defs --------------*/
 typedef enum 
 {
      Operation_Done      =1,
      No_Students         =2,
+     Repeated_ID         =3,
 } DataBase_Status; 
+/*------------- Variables --------------*/
+u8 Global_Course_ID;
+u32 Global_ID;
+DataBase_Status Global_Flag;
+/*-------------- Define ----------------*/
+#define True                  1
+#define False                 0
 /*----------- Functins To Use ---------*/
-
 DataBase_Status Print_All_Students(queue_t *Data_Base);
 DataBase_Status Course_ID_Search(queue_t *Data_Base);
 DataBase_Status Find_Student(queue_t *Data_Base);
+DataBase_Status Check_For_ID(queue_t *Data_Base);
 DataBase_Status Add_Student(queue_t *Data_Base);
 void Check_Course_ID(students_t Student_Data);
 void Print_Student(students_t Student_Data);
+void Get_ID(students_t Student_Data);
 #endif
 /********************************************************************
  *  END OF FILE: Main.h
